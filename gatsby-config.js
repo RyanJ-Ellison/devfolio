f4355c65e0baf524c09a0608cfd6b08d91783008
Ryak0 <125202653+UniOfYorkKendo@users.noreply.github.com>
2023-07-01 15:14:53 +0100

    changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,19 +3,24 @@ module.exports = {
     // Site URL for when it goes live
     siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Ryan Ellison',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Ryan Ellison | PhD Student`,
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
+    description: `This is some example text. I'll change it later!.`,
     // Optional: Twitter account handle
-    author: `@rfitzio`,
+    author: `@_Ryan_Ellison`,
     // Optional: Github account URL
-    github: `https://github.com/ryanfitzgerald`,
+    github: `https://github.com/RyanJ-Ellison`,
     // Optional: LinkedIn account URL
-    linkedin: `https://www.linkedin.com/in/ryanafitzgerald/`,
+    linkedin: `https://www.linkedin.com/in/ryan-ellison-b5b7b4159/`,
     // Content of the About Me section
-    about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
+    about: `I’m Ryan, a PhD student at the univeristy of York. I’m a naturally curoius, driven person, with a passion for understanding how things work. As a PhD student, 
+    I’m currently researching to try and understand the role of specific mutations in triggering the formation of bladder cancer from healthy tissue. 
+    Once the cancer has formed, can these mutations inform us as to how the cancer will behave, for example what treatments it might respond to?\nI’m also passionate about 
+    science outreach and teaching - I love it when you get a question from someone that shows they’re really engaed in what you’re explaining. When you’re explaining 
+    something to a student, it’s very rewarding when you see them have that “Aha!” moment. I have >40 hours of experience as a Graduate Teaching Assistant, teaching in 
+    lab practicals, problem classes, and marking reports.`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
     projects: [
       {
@@ -131,7 +136,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`, // This color appears on mobile
         display: `minimal-ui`,
-        icon: `src/images/icon.png`,
+        icon: `src/images/Picture_Me.jpg`,
       },
     },
   ],
